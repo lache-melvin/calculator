d7bbdc544fab9d8a57fe0be2c9bb21d5b0b143cc
Lache Melvin <lache.melvin@gmail.com>
2020-02-27 16:17:31 +1300

    ITS A CALCULATOR WHAAAT

--- index.js
+++ index.js
@@ -1,47 +1,66 @@
 /*
-make an array var
+bugs to fix = decimal point with rtl writing lol
+if you click an operator and then equals its bad
 
-Make a tempary variable equal to empty string - to hold numbers
-
-add onclick function to each button
-  within onClick make a var chosenButton that tells us which button is clicked on (Check what .text does)
-
-  if chosenButton is a number (!NsNaN) or a decimal point, add it to temp string var, and display it on a screen
-
-  else if the chosenButton value is equal to AC or CE, than clear temp and screen
-    and if it is AC clear entries
-  
-  else if chosenButton is +,-,* or /
-  
-    operatorFucntion(chosenButton)
-    run a function that does push the temp to the array then push the opporator to the array
-
-  else (meaning it should be euqal to '=')
-    push the temp number to the array
-    make new var called total which we need to make a intiger from the first number from the entries array
-      for loop- make i = 1. less than the array. and go up by two
-      make var Symbol = entries[i]
-      make var nextNum which is equal to intiger of entries i + 1
-
-      if symbol = +; add nextNum to total
-      => for all operators
-    if total < 0
-      make positive and add '-' string
-
-    display the answer!
 
 stretch material  - display answer as operators clicked */
 
 
 var entries = [];
-
+var screen = document.getElementById('screen')
 var tempNum = "";   
 
 var buttonArray = document.getElementsByTagName('button')
+
 for(var i = 0; i < buttonArray.length; i++){
     buttonArray[i].onclick = whichButton
 }
 
-function whichButton(){
-    
+function whichButton(event){
+    var buttonClicked = event.target.innerHTML
+
+    // if entering number
+    if (!isNaN(parseInt(buttonClicked)) || buttonClicked === '.') {
+        tempNum += buttonClicked
+        screen.innerHTML = tempNum.substring(0,10)
+    }
+    // if clearing screen
+    else if (buttonClicked === 'AC' || buttonClicked === 'CE') {
+        if (buttonClicked === 'AC') {
+            entries = [];
+        }
+        tempNum = "";
+        screen.innerHTML = ""
+    }
+
+    // if entering operator
+    else if (buttonClicked === '+' || buttonClicked === '-' || buttonClicked === 'x' || buttonClicked === '÷') {
+        entries.push(tempNum)
+        entries.push(buttonClicked)
+        tempNum = ""
+        
+    }
+
+    // if clicks '='
+    else {
+        entries.push(tempNum)
+        var total = parseInt(entries[0])
+        for (var i = 1; i < entries.length; i += 2) {
+            var symbol = entries[i]
+            var nextNum = parseInt(entries[i+1])
+            if (symbol === '+') {total += nextNum}
+            else if (symbol === '-') {total -= nextNum}
+            else if (symbol === 'x') {total *= nextNum}
+            else if (symbol === '÷') {total /= nextNum}
+        }
+        if (total < 0) {
+            total = Math.abs(total) + '-'
+        }
+        screen.innerHTML = total
+        entries = []
+        tempNum = ""
+    }
+
 }
+
+
