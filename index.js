0b5b76bbc78577e1381bc4d512bbc55ee9196d8c
Lache Melvin <lache.melvin@gmail.com>
2020-03-05 10:51:49 +1300

    remove old evaluation method

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-/*
-if you click an operator and then equals its bad
-straight away equals?
-cursor - move back and forward - 
-calculator memory?
-scientific mode - use my calc - q and ans on screen? - how it displays?
-arrows and numbers as input?
-syntax/math errors
-if operator that wasn't the first operator, delete it
-stretch material  - display answer as operators clicked */
-
+// CALCULATOR :)
 
 var question = "";
 var screen = document.getElementById('screen')
@@ -47,16 +37,6 @@ function whichButton(event){
         question = question.replace("÷", "/")
         var total = eval(question)
 
-        // entries.push(tempNum)
-        // var total = parseFloat(entries[0])
-        // for (var i = 1; i < entries.length; i += 2) {
-        //     var symbol = entries[i]
-        //     var nextNum = parseFloat(entries[i+1])
-        //     if (symbol === '+') {total += nextNum}
-        //     else if (symbol === '-') {total -= nextNum}
-        //     else if (symbol === 'x') {total *= nextNum}
-        //     else if (symbol === '÷') {total /= nextNum}
-        // }
         // if huge number make it x10^x
         if (total > 9999999999) {
             var power = total.toString().length - 1
@@ -84,5 +64,3 @@ function whichButton(event){
         question = ""
     }
 }
-           
-
